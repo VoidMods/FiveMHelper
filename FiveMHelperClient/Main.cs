6352cfe7a393461716941fe7de6f7d752cf5eb13
VoidMods <57848654+VoidMods@users.noreply.github.com>
2020-07-07 15:48:19 +0200

    Rename main class

--- FiveMHelperClient/Main.cs
+++ FiveMHelperClient/Main.cs
@@ -4,33 +4,36 @@ using CitizenFX.Core;
 using static CitizenFX.Core.Native.API;
 
 namespace FiveMHelperClient {
-    public class Class1 : BaseScript {
-        public Class1() {
+    public class Main : BaseScript {
+        // Constants
+        readonly string plateNumber = "OxVoid";
+        readonly int[,] SentinelClassicMods = {
+            { (int)VehicleModType.Spoilers, 3 },
+            { (int)VehicleModType.Suspension, 1 },
+            { (int)VehicleModType.Exhaust, 3 }
+        };
+        readonly int[,] ElegyClassicMods = {
+            { 0, 7 },
+            { 1, 5 },
+            { 3, 3 },
+            { 4, 1 },
+            { 6, 1 },
+            { 7, 5 },
+            { 8, 2 },
+            { 15, 3 },
+            { 25, 0 },
+            { 26, 2 },
+            { 32, 5 },
+            { 33, 7 },
+        };
+
+        public Main() {
             EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
         }
 
         private void OnClientResourceStart(string resourceName) {
+            // Quit if a different resource is started
             if (GetCurrentResourceName() != resourceName) return;
-
-            int[,] SentinelClassicMods = {
-                { (int)VehicleModType.Spoilers, 3 },
-                { (int)VehicleModType.Suspension, 1 },
-                { (int)VehicleModType.Exhaust, 3 }
-            };
-            int[,] ElegyClassicMods = {
-                { 0, 7 },
-                { 1, 5 },
-                { 3, 3 },
-                { 4, 1 },
-                { 6, 1 },
-                { 7, 5 },
-                { 8, 2 },
-                { 15, 3 },
-                { 25, 0 },
-                { 26, 2 },
-                { 32, 5 },
-                { 33, 7 },
-            };
 
             RegisterCommand("car", new Action<int, List<object>, string>(async (source, args, raw) => {
                 // account for the argument not being passed
@@ -66,7 +69,7 @@ namespace FiveMHelperClient {
                     }
                 }
                 // set vanity plate
-                SetVehicleNumberPlateText(vehicleHash, "0xVoid");
+                SetVehicleNumberPlateText(vehicleHash, plateNumber);
                 // set the player ped into the vehicle and driver seat
                 Game.PlayerPed.SetIntoVehicle(vehicle, VehicleSeat.Driver);
             }), false);
@@ -98,9 +101,6 @@ namespace FiveMHelperClient {
                     modelName = args[0].ToString();
                 }
                 switch (modelName) {
-                    case "baywatch":
-                        modelName = "s_f_y_baywatch_01";
-                        break;
                     case "normal":
                         modelName = "s_m_m_ciasec_01";
                         break;
@@ -112,7 +112,6 @@ namespace FiveMHelperClient {
                 Model model = new Model(modelName);
                 await Game.Player.ChangeModel(model);
                 model.MarkAsNoLongerNeeded();
-                //SetPedComponentVariation(PlayerPedId(), 0, 0, 0, 0);
                 SetPedDefaultComponentVariation(PlayerPedId());
             }), false);
         }
